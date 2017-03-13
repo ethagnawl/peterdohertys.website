6b7cf73c05e1cc6d3491c0f5432a776b1df4bb90
Peter Doherty <pdoherty926@gmail.com>
2017-03-13 09:11:25 -0400

    Publish to GitHub Pages

---
+++ deploy.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Shelly
+
+main = shelly $ do
+    run "git" ["stash", "save"]
+    run "stack" ["clean"]
+    run "stack" ["build"]
+    run "git" ["checkout", "master"]
+    run "cp" ["-a", "_site/.", "."]
+    run "git" ["add", "-A"]
+    run "git" ["commit", "-m", "Publish to GitHub Pages"]
+    run "git" ["push", "origin", "master"]
+    run "git" ["checkout", "develop"]
+    run "git" ["stash", "pop"]
+    echo "Finished publishing to GitHub Pages."
